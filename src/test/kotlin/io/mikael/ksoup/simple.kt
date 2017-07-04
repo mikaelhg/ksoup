54e4b7071ccd064a5af6659d5b3bb2357476232e
Mikael Gueck <gumi@iki.fi>
2017-07-05 02:11:40 +0300

    Let's do some utilities, we'll be doing a lot of static file reading.

--- src/test/kotlin/io/mikael/ksoup/simple.kt
+++ src/test/kotlin/io/mikael/ksoup/simple.kt
@@ -1,57 +1,57 @@
 package io.mikael.ksoup
 
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Test
-import okhttp3.mockwebserver.MockWebServer
 
-
-
-class SimpleTests {
+class SimpleTests : StaticWebTest() {
 
     data class GitHubPage(var username: String = "", var fullName: String = "")
 
+    init {
+        contentResolver = {
+            when (it) {
+                "/mikaelhg" -> "github-mikaelhg.html"
+                "/huima" -> "github-huima.html"
+                else -> null
+            }
+        }
+    }
+
     @Test
     fun `simple one page extraction`() {
-        MockWebServer().use { server ->
-            server.enqueue(MockResponse().setBody(resource("github-mikaelhg.html")))
-            val gh = KSoup.extract<GitHubPage> {
-                result { GitHubPage() }
-                url = server.url("/mikaelhg").toString()
-                userAgent = "Mozilla/5.0 UnitTesting/1.0"
-                headers["Accept-Encoding"] = "gzip"
-                element(".p-nickname") { element, page ->
-                    page.username = element.text()
-                }
-                text(".p-name") { text, page ->
-                    page.fullName = text
-                }
+        val gh = KSoup.extract<GitHubPage> {
+            result { GitHubPage() }
+            url = server.url("/mikaelhg").toString()
+            userAgent = "Mozilla/5.0 UnitTesting/1.0"
+            headers["Accept-Encoding"] = "gzip"
+            element(".p-nickname") { element, page ->
+                page.username = element.text()
             }
-            assert(gh.username == "mikaelhg")
-            assert(gh.fullName == "Mikael Gueck")
+            text(".p-name") { text, page ->
+                page.fullName = text
+            }
         }
+        assert(gh.username == "mikaelhg")
+        assert(gh.fullName == "Mikael Gueck")
+        assert(server.takeRequest().path == "/mikaelhg")
     }
 
     @Test
     fun `copy extractors`() {
-        MockWebServer().use { server ->
-            server.enqueue(MockResponse().setBody(resource("github-huima.html")))
-            val ex1 = KSoup.simple<GitHubPage> {
-                result { GitHubPage() }
-                url = "https://github.com/mikaelhg"
-                element(".p-nickname") { element, page ->
-                    page.username = element.text()
-                }
-                text(".p-name") { text, page ->
-                    page.fullName = text
-                }
+        val ex1 = KSoup.simple<GitHubPage> {
+            result { GitHubPage() }
+            url = "https://github.com/mikaelhg"
+            element(".p-nickname") { element, page ->
+                page.username = element.text()
             }
-            val ex2 = ex1.copy(url = server.url("/huima").toString())
-            val gh = ex2.extract()
-            assert(gh.username == "huima")
-            assert(gh.fullName == "Heimo Laukkanen")
+            text(".p-name") { text, page ->
+                page.fullName = text
+            }
         }
+        val ex2 = ex1.copy(url = server.url("/huima").toString())
+        val gh = ex2.extract()
+        assert(gh.username == "huima")
+        assert(gh.fullName == "Heimo Laukkanen")
+        assert(server.takeRequest().path == "/huima")
     }
-
-    internal fun resource(filename: String) = javaClass.classLoader.getResource(filename).readText()
 
 }
--- src/test/kotlin/io/mikael/ksoup/util.kt
+++ src/test/kotlin/io/mikael/ksoup/util.kt
@@ -1,1 +1,44 @@
 package io.mikael.ksoup
+
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import org.junit.After
+import org.junit.Before
+
+internal class StaticDispatcher(private val resolver: (String) -> String?): Dispatcher() {
+
+    private fun readText(filename: String) = javaClass.classLoader.getResource(filename).readText()
+
+    private fun r200() = MockResponse().setResponseCode(200)
+    private fun r404() = MockResponse().setResponseCode(404)
+
+    override fun dispatch(request: RecordedRequest?): MockResponse {
+        val resourcePath = resolver(request!!.path)
+        return when (resourcePath) {
+            null -> r404()
+            else -> r200().setBody(readText(resourcePath))
+        }
+    }
+
+}
+
+open class StaticWebTest {
+
+    protected lateinit var server: MockWebServer
+
+    protected lateinit var contentResolver: (String) -> String?
+
+    @Before
+    fun before() {
+        server = MockWebServer().apply {
+            this.setDispatcher(StaticDispatcher(contentResolver))
+            this.start()
+        }
+    }
+
+    @After
+    fun after(): Unit = server.close()
+
+}
