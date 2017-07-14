12b397a317a8297bc8fc8a17684356f21b04b5f1
Mikael Gueck <gumi@iki.fi>
2017-07-14 16:33:15 +0300

    Logging.

--- src/test/kotlin/io/mikael/ksoup/test/util.kt
+++ src/test/kotlin/io/mikael/ksoup/test/util.kt
@@ -7,8 +7,13 @@ import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import java.util.logging.LogManager
 
 data class GitHubPage(var username: String = "", var fullName: String = "")
+
+internal val resource = StaticWebTest::class.java.classLoader::getResource
+
+internal val resourceAsStream = StaticWebTest::class.java.classLoader::getResourceAsStream
 
 internal class StaticDispatcher(private val resolver: (String) -> String?): Dispatcher() {
 
@@ -16,7 +21,7 @@ internal class StaticDispatcher(private val resolver: (String) -> String?): Disp
         val resourcePath = resolver(request!!.path)
         val resource = when (resourcePath) {
             null -> null
-            else -> javaClass.classLoader.getResource(resourcePath)
+            else -> resource(resourcePath)
         }
         return when (resource) {
             null -> MockResponse().setResponseCode(404)
@@ -31,6 +36,12 @@ open class StaticWebTest {
     protected lateinit var server: MockWebServer
 
     protected lateinit var staticContentResolver: (String) -> String?
+
+    init {
+        resourceAsStream("logging.properties").use {
+            LogManager.getLogManager().readConfiguration(it)
+        }
+    }
 
     @Before
     fun before() {
