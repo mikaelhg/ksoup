b437baad5d47c6a27cf55b6f83a73cd166c75476
Mikael Gueck <gumi@iki.fi>
2020-12-12 22:13:26 +0200

    Java 11 HTTP client.

--- src/main/kotlin/io/mikael/ksoup/http.kt
+++ src/main/kotlin/io/mikael/ksoup/http.kt
@@ -2,7 +2,15 @@ package io.mikael.ksoup
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.net.Authenticator
 import java.net.HttpURLConnection
+import java.net.ProxySelector
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpClient.Builder.NO_PROXY
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
 
 /**
  * The essential basis for all extractors which require access to the web.
@@ -11,7 +19,7 @@ open class WebSupport {
 
     var headers: MutableMap<String, String> = mutableMapOf()
 
-    var httpClient : HttpClient = JdkHttpClient()
+    var soupClient: SoupClient = JavaNetSoupClient()
 
     internal var userAgentGenerator: () -> String = { "Mozilla/5.0 Ksoup/1.0" }
 
@@ -23,8 +31,7 @@ open class WebSupport {
         this.userAgentGenerator = userAgentGenerator
     }
 
-    protected fun get(url: String) : Document
-            = httpClient.get(url, headers, userAgent)
+    protected fun get(url: String): Document = soupClient.get(url, headers, userAgent)
 
 }
 
@@ -32,18 +39,18 @@ open class WebSupport {
  * If you want to use the library with Apache HttpClient, okhttp3, or some other
  * HTTP client implementation, implement a HttpClient and a ResponseToDocumentParser.
  */
-interface HttpClient {
+interface SoupClient {
     fun get(url: String, headers: Map<String, String>, userAgent: String) : Document
 }
 
 /**
  * The most basic, java.net.URL-using HTTP client, with no additional dependencies.
  */
-open class JdkHttpClient(
+open class UrlConnectionSoupClient(
         private val parser: ResponseToDocumentParser<String, Document> = StringToDocumentParser(),
         private val connectTimeout: Int = 1000,
         private val readTimeout: Int = 1000
-) : HttpClient {
+) : SoupClient {
     override fun get(url: String, headers: Map<String, String>, userAgent: String) : Document {
         val con = java.net.URL(url).openConnection() as HttpURLConnection
         headers.forEach(con::setRequestProperty)
@@ -57,6 +64,39 @@ open class JdkHttpClient(
         con.inputStream.use {
             return parser.parse(it.reader().readText())
         }
+    }
+}
+
+/**
+ * Java 11 introduced a much better stock HTTP client.
+ */
+open class JavaNetSoupClient(
+    private val parser: ResponseToDocumentParser<String, Document> = StringToDocumentParser(),
+    private val connectTimeout: Duration = Duration.ofSeconds(1),
+    private val readTimeout: Duration = Duration.ofSeconds(1),
+    private val proxySelector: ProxySelector = NO_PROXY,
+    private val authenticator: Authenticator? = Authenticator.getDefault(),
+    private val redirects: HttpClient.Redirect = HttpClient.Redirect.NORMAL,
+    private val httpVersion: HttpClient.Version = HttpClient.Version.HTTP_1_1
+) : SoupClient {
+    override fun get(url: String, headers: Map<String, String>, userAgent: String): Document {
+        val client = HttpClient.newBuilder()
+            .apply {
+                connectTimeout.let(this::connectTimeout)
+                httpVersion.let(this::version)
+                redirects.let(this::followRedirects)
+                authenticator?.let(this::authenticator)
+                proxySelector.let(this::proxy)
+            }
+            .build()
+        val request = HttpRequest.newBuilder().GET().uri(URI.create(url))
+            .apply {
+                readTimeout.let(this::timeout)
+                headers.forEach(this::header)
+            }
+            .build()
+        val response = client.send(request, BodyHandlers.ofString())
+        return parser.parse(response.body())
     }
 }
 
