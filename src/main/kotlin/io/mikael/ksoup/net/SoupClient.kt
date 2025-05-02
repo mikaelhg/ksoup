70da06db9d8f20793ca3aaaac2c1dc5a8e9182cf
Mikael Gueck <gumi@iki.fi>
2025-05-03 02:09:55 +0300

    refactor: Functional interface

--- src/main/kotlin/io/mikael/ksoup/net/SoupClient.kt
+++ src/main/kotlin/io/mikael/ksoup/net/SoupClient.kt
@@ -6,6 +6,6 @@ import org.jsoup.nodes.Document
  * If you want to use the library with Apache HttpClient, okhttp3, or some other
  * HTTP client implementation, implement a HttpClient and a ResponseToDocumentParser.
  */
-interface SoupClient {
+fun interface SoupClient {
     fun get(url: String, headers: Map<String, String>, userAgent: String): Document
 }
