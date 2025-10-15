2eaf77a164e57175be2977d99d4f3e3336e2cfea
Mikael Gueck <gumi@iki.fi>
2025-10-15 22:21:52 +0300

    upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    kotlin("jvm") version "2.2.20"
+    id("org.jetbrains.kotlinx.kover") version "0.9.2"
 }
 
 group = "io.mikael.ksoup"
@@ -23,11 +23,11 @@ repositories {
 dependencies {
     implementation(platform(kotlin("bom")))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jsoup:jsoup:1.20.1")
-    testImplementation("io.undertow:undertow-core:2.3.18.Final")
-    testImplementation("org.junit.platform:junit-platform-suite-engine:1.12.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.2")
+    implementation("org.jsoup:jsoup:1.21.2")
+    testImplementation("io.undertow:undertow-core:2.3.20.Final")
+    testImplementation("org.junit.platform:junit-platform-suite-engine:6.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.0")
 }
 
 tasks.withType<Test> {
