032164441f08d05303c945d701e23362e8ef9614
Mikael Gueck <gumi@iki.fi>
2025-11-07 00:35:02 +0200

    chore: add CI/CD publish on release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,13 @@ plugins {
     kotlin("jvm") version "2.2.21"
 }
 
-group = "io.mikael.ksoup"
+group = "io.mikael"
 version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
-    withJavadocJar()
     withSourcesJar()
 }
 
@@ -20,8 +19,6 @@ repositories {
 }
 
 dependencies {
-    implementation(platform(kotlin("bom")))
-    implementation(kotlin("stdlib-jdk8"))
     implementation("org.jsoup:jsoup:1.21.2")
     testImplementation("io.undertow:undertow-core:2.3.20.Final")
     testImplementation(kotlin("test"))
@@ -37,10 +34,40 @@ tasks.withType<Test> {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "com.github.mikaelhg"
-            artifactId = "ksoup"
-            version = project.version.toString()
             from(components["java"])
+            pom {
+                name = "ksoup"
+                description = "JSoup DSL for Kotlin"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "mikaelhg"
+                        name = "Mikael Gueck"
+                        email = "gumi@iki.fi"
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mikaelhg/ksoup")
+            credentials {
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("GPR_USER")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("GPR_TOKEN")
+            }
+        }
+        maven {
+            name = "local"
+            url = uri(layout.buildDirectory.dir("repo"))
         }
     }
 }
